d05ed6debf40bc520dec9b2ca998dc76494d38a9
Yuriy Korman <yuriy.korman@sigmaukraine.com>
2017-03-07 19:59:22 +0200

    memberid page

--- app/Resources/src/fenglin/consumer/models/ConsumerModel.js
+++ app/Resources/src/fenglin/consumer/models/ConsumerModel.js
@@ -5,7 +5,7 @@
 define(['backbone'], function(Backbone){
     var reg = /\?apikey=([\w\W]+)/;
     var match = reg.exec(window.location.search);;
-
+    alert(match[1]);
     return Backbone.Model.extend({
         urlRoot: Routing.generate('panda_consumer_rest_index', {
             apikey: match[1]
