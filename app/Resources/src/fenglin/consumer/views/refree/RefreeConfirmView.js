b37e0b57cca041678fe8a37682da5eb2cb39e087
korman <korman.yuri@gmail.com>
2017-03-29 00:33:14 +0300

    added validate new shopper in admin

--- app/Resources/src/fenglin/consumer/views/refree/RefreeConfirmView.js
+++ app/Resources/src/fenglin/consumer/views/refree/RefreeConfirmView.js
@@ -2,7 +2,7 @@
  * Created by korman on 27.01.17.
  */
 
-define(['marionette'], function(Marionette){
+define(['marionette', 'consumer/views/core/MenuView'], function(Marionette, MenuView){
     return Marionette.View.extend({
         el:'#contentContainer',
         template: '#refreeConfirmView',
@@ -12,6 +12,9 @@ define(['marionette'], function(Marionette){
             $('#searchBarContainer').empty();
             $('#paginationContainer').empty();
             $('#menuContainer').empty();
+
+            var menu = new MenuView();
+            menu.render();
         }
     });
 });
